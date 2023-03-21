0963ee5489f3f9c99daac9d28d263740d7b681a2
larigens@gmail.com <larigens@gmail.com>
2023-03-20 22:17:47 -0400

    added resume file and data folder with skills and projects data

---
+++ src/data/skills.js
@@ -0,0 +1,22 @@
+export const skills = [
+    {
+        name: "Languages",
+        skillsArr: ["HTML", "CSS", "JavaScript", "SQL"]
+    },
+    {
+        name: "Libraries and Frameworks",
+        skillsArr: ["React", "JQuery", "Boostrap", "Express.js"]
+    },
+    {
+        name: "Tools and Packages",
+        skillsArr: ["Git", "npm", "Webpack", "Babel", "ESLint", "Jest"]
+    },
+    {
+        name: "Databases",
+        skillsArr: ["MySQL", "MongoDB"]
+    },
+    {
+        name: "Backend Technologies",
+        skillsArr: ["Node.js"]
+    }
+]
