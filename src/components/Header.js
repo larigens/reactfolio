bda00e0008771987dcd03deee1a18ae232294e8f
larigens@gmail.com <larigens@gmail.com>
2023-03-21 22:00:00 -0400

    title corresponding to the current section is highlighted

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Navigation from './Navigation';
 
 export default function Header() {
     // useState Hooks to keep track of what page  should be displayed. 
-    const [currentPage, setCurrentPage] = useState("Home"); 
+    const [currentPage, setCurrentPage] = useState("Home");
 
     return (
         <>
@@ -15,12 +15,12 @@ export default function Header() {
                         <Navbar.Toggle aria-controls="navbarSupportedContent" />
                         <Navbar.Collapse id="navbarSupportedContent">
                             <Nav className="me-auto mb-2 mb-lg-0">
-                                <Nav.Link className="nav-link light-shades gradient-hv mt-2 fs-5 fw-semibold" href="#About" onClick={() => setCurrentPage("About")}>About Me</Nav.Link>
-                                <Nav.Link className="nav-link light-shades gradient-hv mt-2 fs-5 fw-semibold" href="#Portfolio" onClick={() => setCurrentPage("Portfolio")}>Portfolio</Nav.Link>
+                                <Nav.Link className={`nav-link light-shades gradient-hv mt-2 fs-5 fw-semibold ${currentPage === "About" ? "gradient-text" : ""}`} href="#About" onClick={() => setCurrentPage("About")}>About Me</Nav.Link>
+                                <Nav.Link className={`nav-link light-shades gradient-hv mt-2 fs-5 fw-semibold ${currentPage === "Portfolio" ? "gradient-text" : ""}`} href="#Portfolio" onClick={() => setCurrentPage("Portfolio")}>Portfolio</Nav.Link>
                             </Nav>
                             <Nav className="ms-auto mb-2 mb-lg-0 justify-content-end">
-                                <Nav.Link className="nav-link light-shades gradient-hv mt-2 fs-5 fw-semibold" href="#Contact" onClick={() => setCurrentPage("Contact")}>Contact Me</Nav.Link>
-                                <Nav.Link className="nav-link light-shades gradient-hv mt-2 fs-5 fw-semibold" href="#Resume" onClick={() => setCurrentPage("Resume")}>Resume</Nav.Link>
+                                <Nav.Link className={`nav-link light-shades gradient-hv mt-2 fs-5 fw-semibold ${currentPage === "Contact" ? "gradient-text" : ""}`} href="#Contact" onClick={() => setCurrentPage("Contact")}>Contact Me</Nav.Link>
+                                <Nav.Link className={`nav-link light-shades gradient-hv mt-2 fs-5 fw-semibold ${currentPage === "Resume" ? "gradient-text" : ""}`} href="#Resume" onClick={() => setCurrentPage("Resume")}>Resume</Nav.Link>
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
